3c0971fa37a99b18f6a1e1bcbdf2162472918991
Danielle McLean <danielle@ethicaljobs.com.au>
2022-08-02 15:33:07 +1000

    Fix types in RequestUtil

--- src/RequestUtil.php
+++ src/RequestUtil.php
@@ -2,7 +2,7 @@
 
 namespace EthicalJobs\Utilities;
 
-use Exception;
+use Illuminate\Http\Request;
 
 class RequestUtil
 {
@@ -13,12 +13,16 @@ class RequestUtil
      *  a) used a global method (??) and
      *  b) thus binds this generic PHP package to Laravel
      * so deprecating now for next version to remove
+     *
+     * @return list<string>
      */
     public static function getSelectFields(): array
     {
         $grossGlobalMethod = request();
+        assert($grossGlobalMethod instanceof Request);
         $select = $grossGlobalMethod->input('fields');
         if (isset($select) === true) {
+            assert(is_string($select));
             $httpRequestSelectFields = explode(',', $select);
 
             if (str_contains($select, '*')) {
