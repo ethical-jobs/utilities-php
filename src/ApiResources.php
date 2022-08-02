aeff41d2e8350111cf241c21b6293c86705f7bd5
Danielle McLean <danielle@ethicaljobs.com.au>
2022-08-02 15:07:03 +1000

    Fix types in ApiResources

--- src/ApiResources.php
+++ src/ApiResources.php
@@ -2,51 +2,63 @@
 
 namespace EthicalJobs\Utilities;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class ApiResources
 {
+    public const JOBS = 'jobs';
+    public const ORGANISATIONS = 'organisations';
+    public const USERS = 'users';
+    public const MEDIA = 'media';
+    public const INVOICES = 'invoices';
+    public const TAXONOMIES = 'taxonomies';
+    public const ROLES = 'roles';
+    public const CREDITS = 'credits';
+
     /**
      * Returns model class from a REST resource identifier
      *
-     * @param String $resource
-     * @return String
+     * @param self::* $resource
+     * @return class-string<\Illuminate\Database\Eloquent\Model>|""
      */
-    public static function getModelFromResource($resource)
+    public static function getModelFromResource(string $resource): string
     {
         if (!in_array($resource, static::getResources())) {
             return '';
         }
 
-        return 'App\Models\\' . Str::studly(Str::singular($resource));
+        $class = 'App\Models\\' . Str::studly(Str::singular($resource));
+        assert(is_subclass_of($class, Model::class));
+        return $class;
     }
 
     /**
      * Returns API resources
      *
-     * @return array
+     * @return list<self::*>
      */
-    public static function getResources()
+    public static function getResources(): array
     {
         return [
-            'jobs',
-            'organisations',
-            'users',
-            'media',
-            'invoices',
-            'taxonomies',
-            'roles',
-            'credits',
+            self::JOBS,
+            self::ORGANISATIONS,
+            self::USERS,
+            self::MEDIA,
+            self::INVOICES,
+            self::TAXONOMIES,
+            self::ROLES,
+            self::CREDITS,
         ];
     }
 
     /**
      * Returns transformer class from a REST resource identifier
      *
-     * @param String $resource
-     * @return String
+     * @param self::* $resource
+     * @return class-string|""
      */
-    public static function getTransformerFromResource($resource)
+    public static function getTransformerFromResource(string $resource): string
     {
         if (!in_array($resource, static::getResources())) {
             return '';
@@ -54,6 +66,8 @@ class ApiResources
 
         $resourceName = Str::studly(Str::singular($resource));
 
-        return 'App\Transformers\\' . $resourceName . 's\\' . $resourceName . 'Transformer';
+        $class = 'App\Transformers\\' . $resourceName . 's\\' . $resourceName . 'Transformer';
+        assert(class_exists($class));
+        return $class;
     }
 }
